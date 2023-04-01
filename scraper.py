8d1c3ebe114c0fea5322b8424124ca533eff207f
Carlos Estevez <carlos973.cm@gmail.com>
2023-04-01 14:38:49 -0300

    listo para publicar

--- scraper.py
+++ scraper.py
@@ -1,9 +1,16 @@
-import requests
+from airflow.operators.python import PythonOperator
+from airflow.sensors.filesystem import FileSensor
+from datetime import datetime, timedelta, date
+from airflow.models import Variable
+from airflow import DAG
 import lxml.html as html
+import requests
 import os
-import datetime
 
-HOME_URL = 'https://www.larepublica.co/'
+
+HOME_URL = Variable.get('home_url')
+SLACK_WEBHOK = Variable.get('slack_webhook')
+FILE_PATH_OUTPUT = '/home/airflow/gcs/data/output/'
 
 XPATH_LINK_TO_ARTICLE = '//text-fill/a/@href'
 XPATH_TITLE = '//div[@class="mb-auto"]/h2/span/text()'
@@ -20,13 +27,13 @@ def parse_notice(link,today):
 
             try:
                 title = parsed.xpath(XPATH_TITLE)[0]
-                title = title.replace('\"','')
+                title = title.replace('\n','')
                 summary = parsed.xpath(XPATH_SUMMARY)[0]
                 body = parsed.xpath(XPATH_BODY)
             except IndexError:
                 return
 
-            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
+            with open(f'{FILE_PATH_OUTPUT+today}/{title}.txt', 'w', encoding='utf-8') as f:
                 f.write(title)
                 f.write('\n\n')
                 f.write(summary)
@@ -45,7 +52,7 @@ def parse_notice(link,today):
         print(ve)
 
 
-def parse_home():
+def parse_home(**kwargs):
     try:
         response = requests.get(HOME_URL)
         if response.status_code == 200:
@@ -53,25 +60,58 @@ def parse_home():
             parsed = html.fromstring(home)
             links_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE)
 
-            today = datetime.date.today().strftime('%d-%m-%Y')
+            today = date.today().strftime('%d-%m-%Y')
 
-            if not os.path.isdir(today):
-                os.mkdir(today)
+            if not os.path.isdir(FILE_PATH_OUTPUT+today):
+                os.mkdir(FILE_PATH_OUTPUT+today)
             
             for link in links_to_notices:
                 parse_notice(link,today)
-
+            
+            num_notes = len(os.listdir(FILE_PATH_OUTPUT+today))
 
         else: 
             raise ValueError(f'Error: {response.status_code}')
-
-
+        
     except ValueError as ve:
         print(ve)
 
-def run():
-    parse_home()
+    kwargs['ti'].xcom_push(key='number_of_notes', value=num_notes)
 
-if __name__ == '__main__':
-    run()  
+def send_msg(**kwargs):
+    number_of_notes = kwargs['ti'].xcom_pull(key='number_of_notes', task_ids=['parse_home'])[0]
+    text = f"""
+---------------------------------------------   
+{number_of_notes} notas listas para ser analisadas.
+---------------------------------------------
+    """
+    requests.post(SLACK_WEBHOK, json = {'text':text})
+
+# --------------------------------
+# DAG definition
+# --------------------------------
+
+# Define the defualt args
+default_args = {
+    'owner': 'Carlos Estevez',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 3, 31),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'provide_context': True}
+
+# Create the DAG object
+with DAG(
+    dag_id='scraper',
+    description='extrae las notas del diario y envia una alerta cuando ha finalizado',
+    schedule_interval= '0 12 * * *',
+    default_args = default_args,
+    catchup=True
+) as scraper:
+    t1 = PythonOperator(task_id='parse_home',
+                        python_callable=parse_home)
+    t2 = PythonOperator(task_id='send_msg',
+                        python_callable=send_msg)
+# Set the dependencies
+    t1 >> t2  
 
